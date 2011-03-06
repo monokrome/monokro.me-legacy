f00bd281899c4d7d556e45c172cf335f79b416de
Brandon R. Stoner <monokrome@monokro.me>
2011-03-06 06:21:22 -0800

    Updated default settings to use sqlite3

--- monokrome/settings/defaults.py
+++ monokrome/settings/defaults.py
@@ -1,5 +1,7 @@
 # Django settings for fk project.
 
+from monokrome.shortcuts import asset_path
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,12 +13,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': asset_path('db.sqlite3'),       # Or path to database file if using sqlite3.
+        'USER': '',                             # Not used with sqlite3.
+        'PASSWORD': '',                         # Not used with sqlite3.
+        'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
 }
 
