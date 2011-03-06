6d7da0d7447c0fe5d2ce261b06effbadda3e8490
Brandon R. Stoner <monokrome@monokro.me>
2011-03-06 07:14:36 -0800

    Updated codebase to include a View object, as well as to provide a homepage using a View based on this object.

--- monokrome/urls.py
+++ monokrome/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import *
+from monokrome.views import Home
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', Home(), name='home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
