cf0b9df6c3a45d61cf574e8ab96353717fc40bfd
Brandon R. Stoner <monokrome@monokro.me>
2011-03-06 06:14:45 -0800

    Added a module that defines helper methods for getting project paths.

---
+++ monokrome/shortcuts.py
@@ -0,0 +1,18 @@
+def project_path(dirname):
+    """ Gets a path relative to the project's root directory. """
+
+    import os
+
+    if 'VIRTUAL_ENV' is os.environ:
+        project_directory = os.path.abspath(os.environ['VIRTUAL_ENV'])
+    else:
+        project_directory = os.path.expanduser('~')
+
+    return os.sep.join([project_directory, dirname])
+
+def asset_path(dirname, assets_directory='assets'):
+    """ Gets a path under the project's assets directory. """
+
+    import os
+
+    return project_path(os.sep.join([assets_directory, dirname]))
